dc51cfedbd96217bf037867e6ff302ec81740a61
cyantis <cyantis@gmail.com>
2019-05-29 21:37:35 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,4 +32,20 @@ class ApplicationController < Sinatra::Base
     erb :show
   end
 
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.update(params[:article])
+    redirect to "/articles/#{@article.id}"
+  end
+
+  delete '/articles/:id/delete' do
+    Article.destroy(params[:id])
+    redirect '/articles'
+  end
+
 end
