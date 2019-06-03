857e5b5235a2b34d4e792b7b89e0be180efca593
Mike White <michaeljwhite@gmail.com>
2019-06-02 23:34:00 -0700

    Done.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,11 @@
 #Placeholder for a model
 
 class Article < ActiveRecord::Base
+    #attr_accessor :title, :content
+
+    #def initialize(params)
+    #    super
+    #    params.each {|key, value| self.send("#{key}=", value)}
+    #end
+
 end
